ff6d9f54bd7cc58f71c1b5d510e7e5e4c5492e5f
Ben Merrell <ben.c.merrell@gmail.com>
2026-03-29 13:17:33 +0000

    File changes

--- src/components/map/UnadoptedRoadsLayer.jsx
+++ src/components/map/UnadoptedRoadsLayer.jsx
@@ -1,16 +1,3 @@
-import { Polyline } from 'react-leaflet';
-
 export default function UnadoptedRoadsLayer({ roads }) {
-  if (!roads || roads.length === 0) return null;
-  return (
-    <>
-      {roads.map((positions, i) => (
-        <Polyline
-          key={i}
-          positions={positions}
-          pathOptions={{ color: '#ef4444', weight: 3, opacity: 0.85 }}
-        />
-      ))}
-    </>
-  );
+  return null;
 }
