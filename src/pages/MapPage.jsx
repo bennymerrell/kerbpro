af7e9f673953cd562a505b7ad162e4963898c06f
Ben Merrell <ben.c.merrell@gmail.com>
2026-03-30 07:04:00 +0000

    File changes

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -260,7 +260,7 @@ export default function MapPage() {
       </MapContainer>
 
       {/* Search */}
-      <div className="absolute top-4 left-14 right-14 z-[999] flex items-center">
+      <div className="absolute top-4 left-20 right-20 z-[999] flex items-center">
         <SearchBox onLocationFound={handleLocationFound} />
       </div>
 
