cb135314666ae326af9ba3ec566501894c9d02b4
Ben Merrell <ben.c.merrell@gmail.com>
2026-04-01 17:10:37 +0000

    File changes

--- src/lib/mapUtils.js
+++ src/lib/mapUtils.js
@@ -78,7 +78,7 @@ export const TILE_LAYERS = {
     name: "Satellite",
     url: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
     attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
-    maxZoom: 19,
+    maxZoom: 21,
   },
   terrain: {
     name: "Terrain",
