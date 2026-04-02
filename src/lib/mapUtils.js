6ce5a9aab4b1e4a6e149df6a47b24fcb34d252f0
Ben Merrell <ben.c.merrell@gmail.com>
2026-04-02 22:33:17 +0000

    Increased Cell Label Size

--- src/lib/mapUtils.js
+++ src/lib/mapUtils.js
@@ -70,9 +70,9 @@ export function getSegmentDistances(waypoints) {
 export const TILE_LAYERS = {
   osm: {
     name: "Standard",
-    url: "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> | &copy; <a href="https://opentopomap.org/">OpenTopoMap</a>',
-    maxZoom: 17,
+    url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    maxZoom: 19,
   },
   satellite: {
     name: "Satellite",
