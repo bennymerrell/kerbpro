062ff833ab8ceb6c28c2eb235a3526b42bcaae18
Ben Merrell <ben.c.merrell@gmail.com>
2026-04-03 22:20:09 +0000

    File changes

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -252,6 +252,8 @@ export default function MapPage() {
           attribution={currentTile.attribution}
           url={currentTile.url}
           maxZoom={currentTile.maxZoom}
+          tileSize={currentTile.tileSize || 256}
+          zoomOffset={currentTile.zoomOffset || 0}
         />
         <MapClickHandler onMapClick={handleMapClick} isActive={isPlotting || isSpeciesMode || (isAreaMode && !areaClosed)} />
         <LocationWatcher onLocationUpdate={setLocationData} />
