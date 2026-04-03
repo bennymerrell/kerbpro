55fba6b89a05e17cdaaa5e52bfb5ef6ec78549ea
Ben Merrell <ben.c.merrell@gmail.com>
2026-04-03 22:14:22 +0000

    File changes

--- src/pages/PrintMapPage.jsx
+++ src/pages/PrintMapPage.jsx
@@ -153,9 +153,9 @@ export default function PrintMapPage() {
               zoomDelta={0.1}
             >
               <TileLayer
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                maxZoom={19}
+                url="https://api.os.uk/maps/raster/v1/zxy/Road_3857/{z}/{x}/{y}.png?key=7WyHISIynrtLqpEErdgi0CSfalJaux6I"
+                attribution='Contains OS data &copy; Crown copyright and database rights 2024'
+                maxZoom={20}
               />
               <MapContent selectedCell={selectedCell} zoom={zoom} setZoom={setZoom} onMapReady={(m) => { leafletMapRef.current = m; }} />
             </MapContainer>
